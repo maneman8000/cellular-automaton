42078979c42d8bd70444cad2f0b713e66d831e9a
Akihiro Nishiya <akihiro.nishiya@gmail.com>
2019-03-04 12:37:33 +0900

    fix build error

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const path = require(`path`)
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
-  const page = path.resolve(`./src/templates/page.js`)
+  const page = path.resolve(`./src/templates/Page.js`)
   for (let i = 0; i < 256; i++) {
     createPage({
       path: String(i),
